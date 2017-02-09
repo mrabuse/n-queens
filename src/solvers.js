cf9bb59e298017d51e6fb29cdee9e077fd0d5c44
mrabuse <cogglemat@gmail.com>
2017-02-08 20:04:20 -0600

    Add a lot of nonsense trying to get our function to work with backbone

--- src/solvers.js
+++ src/solvers.js
@@ -74,15 +74,72 @@ window.countNRooksSolutions = function(n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
+  var fullBoard = new Board({n: n});
 
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-  return solution;
+  var search = function(board, row) {
+    var Row = board.get(row);
+    console.log("column value", Row);
+
+    for (var col = 0; col < n; col ++) {
+      array = [];
+      for (var i = 0; i < n; i ++) {
+        if (i !== col) {
+          array.push(0);
+        } else {
+          array.push(1);
+        }
+      }
+
+      var newRow = board.set(row, array);
+
+      console.log("newRow", newRow);
+
+      if (!board.hasAnyColConflicts() &&
+          !board.hasAnyRowConflicts() &&
+          !board.hasAnyMajorDiagonalConflicts() &&
+          !board.hasAnyMinorDiagonalConflicts()) {
+        if (row === n - 1) {
+          solution = board.attributes;
+
+          //return solution;
+        } else {
+          search(board, row + 1);
+        }
+      }
+      for (var i = 0; i < n; i++) {
+        array.push(0);
+      }
+      board.set(row, array);
+    }
+  };
+  search(fullBoard, 0);
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+  var fullBoard = new Board({n: n});
+  var board = fullBoard.attributes;
+
+
+  var search = function(matrix, row) {
+    for (var col = 0; col < n; col ++) {
+      matrix[row][col] = 1;
+      if (!fullBoard.hasAnyColConflicts() &&
+          !fullBoard.hasAnyRowConflicts() &&
+          !fullBoard.hasAnyMajorDiagonalConflicts() &&
+          !fullBoard.hasAnyMinorDiagonalConflicts()) {
+        if (row === n - 1) {
+          solutionCount++;
+        } else {
+          search(matrix, row + 1);
+        }
+      }
+      matrix[row][col] = 0;
+    }
+  };
+
+  search(board, 0);
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
